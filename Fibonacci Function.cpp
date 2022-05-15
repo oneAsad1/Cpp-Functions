ee01483e4baf50e6bc97cf31a7393227bb258553
Muhammad Asad Bashir <94337428+Asad34@users.noreply.github.com>
2022-05-15 16:13:32 +0500

    Update Fibonacci Function.cpp

--- Fibonacci Function.cpp
+++ Fibonacci Function.cpp
@@ -1,16 +1,21 @@
-int
-fib (int n)
+//Iterative Function
+int fib (int n)
 {
-  
-      int k1, k2, k3;
-
-      k1 = k2 = k3 = 1;
-
+    int k1, k2, k3;
+    k1 = k2 = k3 = 1;
     for (int j = 3; j <= n; j++)
-	     {
+	{
            k3 = k1 + k2;
            k1 = k2;
            k2 = k3;
-                 }
+                    }
     return k3;
+}
+//Recursive Function
+int fibon(int n)
+{
+    if (n == 1 || n == 2) 
+	  return 1;
+    else 
+	  return (fib(n - 1) + fib(n - 2));
 }
